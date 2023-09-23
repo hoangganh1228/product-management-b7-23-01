2d32ce9fa2a4bd1a7c4b49e550ba359cdcde7dfe
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-09-23 10:17:45 +0700

    creat middlewares

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const multer = require('multer');
+
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter")
-const upload = multer({storage: storageMulter()});
+
+
+
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 
 router.get("/", controller.index);
 
@@ -20,9 +25,11 @@ router.get("/create", controller.create);
 router.post(
     "/create",
     upload.single('thumbnail'),
+    uploadCloud.upload,
     validate.createPost,
     controller.createPost
-);
+)
+ 
 
 router.get("/edit/:id", controller.edit);
 
