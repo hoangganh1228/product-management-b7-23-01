a405096280fe3c36aefb058aaef140a0c5d1eb94
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-09-30 16:19:25 +0700

    bai 20 buoi 2 lan 2

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -2,31 +2,20 @@ const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system")
 
+const createTreeHelper = require("../../helpers/createTree")
+
 // [GET] /admin/product-category
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
 
-  const createTree = (arr, parentId = "") => {
-    const tree = [];
-    arr.forEach((item) => {
-      if (item.parent_id === parentId) {
-        const newItem = item;
-        const children = createTree(arr, item.id);
-        if (children.length > 0) {
-          newItem.children = children;
-        }
-        tree.push(newItem);
-      }
-    });
-    return tree;
-  }
+
 
 
   const records = await ProductCategory.find(find);
 
-  const newRecords = createTree(records);
+  const newRecords = createTreeHelper.tree(records);
 
   res.render("admin/pages/products-category/index", {
     pageTitle: "Danh mục sản phẩm",
